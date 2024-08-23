5e2915d50154984cbbe98784d22c760fb29577b8
Ayaan-Mi <ayaanmittal39@gmail.com>
2024-08-23 20:01:32 +0530

    updated

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// calculator
+
 let displayValue = "";
 
 function number(num) {
@@ -30,33 +32,30 @@ function UpdateDisplay() {
    document.getElementById('display').innerText = displayValue;
 }
 
-function updateBase() {
-  const num = document.getElementById('base').value;
-  document.getElementById('num').innerHTML = num
-}
-
-function updateExponent() {
-  const pow = document.getElementById('exponent').value;
-  document.getElementById('pow').innerHTML = pow
-}
-
+// Function to calculate power
 function calculatePower() {
   const num = parseFloat(document.getElementById('base').value);
   const pow = parseFloat(document.getElementById('exponent').value);
-  const ans = Math.pow(num , pow)
-  document.getElementById('ans').innerHTML = ans;
+
+  if (isNaN(num) || isNaN(pow)) {
+      document.getElementById('power_result').innerText = "Please enter valid numbers";
+      return;
+  }
+
+  const result = Math.pow(num, pow);
+  document.getElementById('power_result').innerText = result;
 }
 
-function calculatert() {
-    const number = parseFloat(document.getElementById('rt_num').value);
-    const root = parseFloat(document.getElementById('root').value);
+// Function to calculate root
+function calculateRoot() {
+  const num = parseFloat(document.getElementById('rt_num').value);
+  const root = parseFloat(document.getElementById('root').value);
 
-    if (isNaN(number) || isNaN(root)) {
-        document.getElementById('sqrt_result').innerText = "Please enter valid numbers";
-        return;
-    }
+  if (isNaN(num) || isNaN(root) || root <= 0) {
+      document.getElementById('sqrt_result').innerText = "Please enter valid numbers and root greater than 0";
+      return;
+  }
 
-    const result = Math.pow(number, 1 / root);
-
-    document.getElementById('sqrt_result').innerText = result;
+  const result = Math.pow(num, 1 / root);
+  document.getElementById('sqrt_result').innerText = result;
 }
